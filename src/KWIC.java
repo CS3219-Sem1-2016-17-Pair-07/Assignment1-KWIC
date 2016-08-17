874cd6294099640b70fc5cf23f105e0661804b79
ymymym <ngyongming@msn.com>
2016-08-17 23:21:54 +0800

    Init

---
+++ src/KWIC.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class KWIC {
+	
+	public static void main(String [] args){
+		
+	}
+	
+	public ArrayList<String> circularShift(ArrayList<String> fromInput){
+		return null;
+	}
+	
+	public ArrayList<String> alphabetizer(ArrayList<String> fromCircularShift){
+		return null;
+	}
+	
+	public void printOutput(ArrayList<String> fromAlphabetizer){
+		for(int i = 0 ; i < fromAlphabetizer.size(); i++){
+			System.out.println(fromAlphabetizer.get(i));
+		}
+	}
+}

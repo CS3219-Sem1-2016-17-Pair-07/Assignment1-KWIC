022671d88b3903c20fc88cd315b934e3700778c4
ymymym <ngyongming@msn.com>
2016-08-24 19:25:02 +0800

    Capital keyword

--- src/PipeFilter/Alphabetizer.java
+++ src/PipeFilter/Alphabetizer.java
@@ -17,8 +17,23 @@ public class Alphabetizer extends Thread {
 		Information info = inPipe.read();
 		ArrayList<String> temp = info.getLines();
 		Collections.sort(temp);
+	    temp = capKeywords(temp);
 		outPipe.write(new Information(temp, info.getIgnoreWords()));
 	}	
+	
+	private ArrayList<String> capKeywords(ArrayList<String> lines){
+		for(int i=0; i<lines.size(); i++){
+			String tempStr = lines.get(i);
+			lines.set(i, tempStr.substring(0, 1));
+			/*
+			if(tempStr.length() > 1){
+				lines.set(i, tempStr.substring(0, 1).toUpperCase());
+			} else {
+				lines.set(i, tempStr.substring(0, 1).toUpperCase());
+			}*/
+		}
+		return lines;
+	}
 	
 	public void run() {
 		while(true){
